41b53a8bb6abb03b842e27303f66b06135034116
Adam Boddington <adam@boddington.net>
2020-12-26 17:47:07 +1100

    chore: add testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,15 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "prettier",
+  ],
   parserOptions: {
     ecmaVersion: 2020,
   },
+  plugins: ["jest"],
   root: true,
 };
